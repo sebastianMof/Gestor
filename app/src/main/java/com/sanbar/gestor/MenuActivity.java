23f77eb6cc99b5b392375fda456eb70695287d8d
Sebastian <sebastian.mofre@gmail.com>
2019-09-20 12:29:36 -0300

    personas update

--- app/src/main/java/com/sanbar/gestor/MenuActivity.java
+++ app/src/main/java/com/sanbar/gestor/MenuActivity.java
@@ -2,8 +2,13 @@ package com.sanbar.gestor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import com.sanbar.gestor.ui.login.LoginActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -21,5 +26,23 @@ public class MenuActivity extends AppCompatActivity {
             String loginModelData1 = extras.getString("loginModelData1");//model.getDisplayName()
         }
 
+        configureButtonPersonas();
+
     }
+
+    private void configureButtonPersonas() {
+        Button btn_personas = (Button) findViewById(R.id.menu_personas);
+        btn_personas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MenuActivity.this, PersonasActivity.class);
+                myIntent.putExtra("loginModelData1", "dato"); //Optional parameters
+                MenuActivity.this.startActivity(myIntent);
+
+            }
+        });
+    }
+
+
+
 }
---
+++ app/src/main/java/com/sanbar/gestor/PersonasActivity.java
@@ -0,0 +1,21 @@
+package com.sanbar.gestor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PersonasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personas);
+    }
+
+
+
+
+}
