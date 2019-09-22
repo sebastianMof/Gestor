af455b0f829995c5644b9dd16514308daf7df442
Sebastian <sebastian.mofre@gmail.com>
2019-09-22 18:46:22 -0300

    before list

--- app/src/main/java/com/sanbar/gestor/MenuActivity.java
+++ app/src/main/java/com/sanbar/gestor/MenuActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,9 +18,6 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        String welcome = getString(R.string.welcome);
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-
         Bundle extras = getIntent().getExtras();
         if(extras !=null)
         {
@@ -31,13 +29,14 @@ public class MenuActivity extends AppCompatActivity {
     }
 
     private void configureButtonPersonas() {
+
         Button btn_personas = (Button) findViewById(R.id.menu_personas);
         btn_personas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(MenuActivity.this, PersonasActivity.class);
                 myIntent.putExtra("loginModelData1", "dato"); //Optional parameters
-                MenuActivity.this.startActivity(myIntent);
+                startActivity(myIntent);
 
             }
         });
--- app/src/main/java/com/sanbar/gestor/PersonasActivity.java
+++ app/src/main/java/com/sanbar/gestor/PersonasActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class PersonasActivity extends AppCompatActivity {
 
@@ -13,6 +15,17 @@ public class PersonasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personas);
+        configureSpinner();
+    }
+
+    private void configureSpinner(){
+
+        Spinner dropdown = findViewById(R.id.spinner_personas);
+        String[] items = new String[]{"TIPO", "TIPO_1", "TIPO_2", "TIPO_3"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+
+        dropdown.setAdapter(adapter);
     }
 
 
